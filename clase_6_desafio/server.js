89eb7cfaf202bdddc4b15f11857b7af6c48d2c61
Fernando Rossler <frossler89@gmail.com>
2022-09-01 09:57:40 -0300

    Update

--- clase_6_desafio/server.js
+++ clase_6_desafio/server.js
@@ -22,7 +22,7 @@ app.get('/productorandom', async (req, res)=> {
     const products = await container.getAll().then(res=>res)
     const randomProd = Math.floor(Math.random()*products.lenght)
     const showRandom = products[randomProd]
-    res.json(`RANDOM PRODUCT: ${JSON.stringify(showRandom)}`) // randomProd = null // showRandom = undefined
+    res.send(`RANDOM PRODUCT: ${JSON.stringify(showRandom)}`) // randomProd = null // showRandom = undefined
 })
 
 app.get('/*', (req, res)=> {
